9bcde74fe0398b20f0a046a8b430f46eae43befe
Gabriel Barbosa <gabrielosantosb@gmail.com>
2024-07-05 16:12:17 -0300

    feat: adicionando fluent validation

---
+++ MyRecipeBook.Application/UseCases/User/Register/RegisteredUserCase.cs
@@ -0,0 +1,21 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisteredUserCase
+    {
+        public ResponseRegisteredUser Execute(RequestRegisterUser request)
+        {
+            return new ResponseRegisteredUser()
+            {
+                Name = request.Name,
+            };
+        }
+
+    }
+    private void Validate(RequestRegisterUser request)
+    {        
+        
+    }
+}
--- MyRecipeBook.Communication/Requests/RequestRegisteredUser.cs
+++ MyRecipeBook.Communication/Requests/RequestRegisteredUser.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyRecipeBook.Communication.Requests
 {
-    public class RequestRegisteredUser
+    public class RequestRegisterUser
     {
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
