078f3cb429b45cf056a58ec26e1dfdd442edb0ce
Gabriel Barbosa <gabrielosantosb@gmail.com>
2024-07-08 15:57:26 -0300

    feat: adding middleware and cultureInfo

--- MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+
 
 namespace MyRecipeBook.Application.UseCases.User.Register
 {
@@ -7,10 +9,10 @@ namespace MyRecipeBook.Application.UseCases.User.Register
     {
         public RegisterUserValidator() 
         { 
-            RuleFor(user => user.Name).NotEmpty();
-            RuleFor(user => user.Email).NotEmpty();
-            RuleFor(user => user.Email).EmailAddress();
-            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+            RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
+            RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_EMPTY);
         }
     }
 }
